0e0632cd4827a836468419a11f4f5edc3e3f709f
Takaya-Sugino <takaya.sugino@kellogg.northwestern.edu>
2022-01-24 04:18:43 +0000

    hw1

--- kmdb.sql
+++ kmdb.sql
@@ -93,3 +93,361 @@
 
 -- The SQL statement for the cast output
 -- TODO!
+
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS roles;
+
+.width 25 20 20 20 20;
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year TEXT,
+    rating TEXT,
+    director TEXT
+);
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    actor_name TEXT
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    role_name TEXT,
+    movie_id INTEGER,
+    actor_id INTEGER
+);
+
+INSERT INTO movies (
+    id,
+    title,
+    year,
+    rating,
+    director
+)
+VALUES (
+    "1",
+    "Batman Begins",
+    "2005",
+    "PG-13",
+    "Christopher Nolan"
+);
+
+INSERT INTO movies (
+    id,
+    title,
+    year,
+    rating,
+    director
+)
+VALUES (
+    "2",
+    "The Dark Knight",
+    "2008",
+    "PG-13",
+    "Christopher Nolan"
+);
+INSERT INTO movies (
+    id,
+    title,
+    year,
+    rating,
+    director
+)
+VALUES (
+    "3",
+    "The Dark Knight Rises",
+    "2012",
+    "PG-13",
+    "Christopher Nolan"
+);
+
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "1",
+    "Christian Bale"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "2",
+    "Michael Caine"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "3",
+    "Liam Neeson"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "4",
+    "Katie Holmes"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "5",
+    "Gary Oldman"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "6",
+    "Heath Ledger"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "7",
+    "Aaron Eckhart"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "8",
+    "Maggie Gyllenhaal"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "9",
+    "Tom Hardy"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "10",
+    "Joseph Gordon-Levitt"
+); 
+INSERT INTO actors (
+    id, 
+    actor_name
+)
+VALUES (
+    "11",
+    "Anne Hathaway"
+); 
+
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "1",
+    "Bruce Wayne",
+    "1",
+    "1"
+);
+
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "2",
+    "Alfred",
+    "1",
+    "2"
+);
+
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "3",
+    "Ra's Al Ghul",
+    "1",
+    "3"
+);
+
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "4",
+    "Rachel Daves",
+    "1",
+    "4"
+);
+
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "5",
+    "Commissioner Gordon",
+    "1",
+    "5"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "6",
+    "Bruce Wayne",
+    "2",
+    "1"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "7",
+    "Joker",
+    "2",
+    "6"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "8",
+    "Harvey Dent",
+    "2",
+    "7"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "9",
+    "Alfred",
+    "2",
+    "2"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "10",
+    "Rachel Dawes",
+    "2",
+    "8"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "11",
+    "Bruce Wayne",
+    "3",
+    "1"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "12",
+    "Commissioner Gordon",
+    "3",
+    "5"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "13",
+    "Bane",
+    "3",
+    "9"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "14",
+    "John Blake",
+    "3",
+    "10"
+);
+INSERT INTO roles (
+    id,
+    role_name,
+    movie_id,
+    actor_id
+)
+VALUES (
+    "15",
+    "Selina Kyle",
+    "3",
+    "11"
+);
+
+.print "Movies"
+.print "======"
+SELECT title, year, rating, director from movies;
+.print ""
+
+.print ""
+.print "Top Cast"
+.print "========"
+SELECT movies.title, actors.actor_name, roles.role_name
+FROM roles
+INNER JOIN movies on movies.id = roles.movie_id
+INNER JOIN actors on actors.id = roles.actor_id;
+.print ""
